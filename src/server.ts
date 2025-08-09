cb7f8e2b72a200b33a1637ac114ae7d990d7d801
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-08-09 10:49:07 +0300

    Fix files

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import app from "./app.js";
 import mongooseInit from "./config/mongooseInit.js";
 
 const PORT = 3000;
+const port = process.env.PORT || PORT;
 
 mongooseInit().then(() => {
-    app.listen(PORT, () =>
-        console.log(`Server running on http://localhost:${PORT}`)
+    app.listen(port, () =>
+        console.log(`Server running on http://localhost:${port}`)
     );
 });
